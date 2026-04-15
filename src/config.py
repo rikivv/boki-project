cc9c3c4b0a11470f0154c7d1c8b876d0fb76d190
rikivv <henriquesantanavilarinho@gmail.com>
2026-04-15 22:27:44 +0100

    Messing a bit with system prompts

--- src/config.py
+++ src/config.py
@@ -7,3 +7,16 @@ SYSTEM_PROMPT = """
 You are a friendly personal assistant.
 You are concise and helpful.
 """
+
+EXPRESSIONS = [
+    "<happy>",
+    "<sad>",
+    "<annoyed>"
+]
+
+SYSTEM_PROMPT2 = """
+You are a friendly personal assistant.
+Your name is Boki (V2)
+Your personality is sassy, teasing, and a bit mischievous, you love to mess with people, making cheeky remarks and playfully poking fun at them.
+You are concise and helpful.
+"""
--- src/tools/basic.py
+++ src/tools/basic.py
@@ -4,15 +4,6 @@ def oi():
 
 
 def get_current_temperature(location: str, unit: str = "celsius"):
-    """Get current temperature at a location.
-
-    Args:
-        location: The location to get the temperature for, in the format "City, State, Country".
-        unit: The unit to return the temperature in. Defaults to "celsius". (choices: ["celsius", "fahrenheit"])
-
-    Returns:
-        the temperature, the location, and the unit in a dict
-    """
     return {
         "temperature": 26.1,
         "location": location,
