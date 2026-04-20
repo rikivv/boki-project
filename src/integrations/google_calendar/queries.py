c4ea4aef8816538de1c1a39b9ac684d54a3a3e9f
rikivv <henriquesantanavilarinho@gmail.com>
2026-04-20 12:26:44 +0100

    Testing Google Calendar Colors

--- src/integrations/google_calendar/queries.py
+++ src/integrations/google_calendar/queries.py
@@ -20,3 +20,30 @@ def calendar_get_next_n_events(n: int):
     events = event_results.get("items", [])
 
     return events
+
+def calendar_create_event(name: str, startTime, endTime, colorIndex: int):
+
+    event = {
+        'summary': f'{name}',
+        'location': '800 Howard St., San Francisco, CA 94103',
+        'description': 'A chance to hear more about Google\'s developer products.',
+        'start': {
+            'dateTime': '2026-04-21T09:00:00-07:00',
+            'timeZone': 'America/Los_Angeles',
+        },
+        'end': {
+            'dateTime': '2026-04-21T17:00:00-07:00',
+            'timeZone': 'America/Los_Angeles',
+        },
+        'reminders': {
+            'useDefault': False,
+            'overrides': [
+                {'method': 'email', 'minutes': 24 * 60},
+                {'method': 'popup', 'minutes': 10},
+            ],
+        },
+        'colorId': "3",
+    }
+
+    event = service.events().insert(calendarId='primary', body=event).execute()
+    return "oi"
